46e397077342e62260e96c8836b69793bc229339
Abu Hasnat <hasnat.tushar@gmail.com>
2023-10-23 04:05:18 -0500

    Add project files.

---
+++ Abu_Hasnat_SD_350_Lab_1/Program.cs
@@ -0,0 +1,65 @@
+﻿public abstract class Client
+{
+    public string Name {  get; set; }
+    public string Email {  get; set; }
+    public int Age { get; set; }
+    public bool AccessDisabled { get; set; }
+    protected IAccessHandler _accessHandler;
+
+    public virtual bool HandleAccess()
+    {
+
+        return _accessHandler.GetAccess(accessDisabled: AccessDisabled);
+    }
+}
+
+public class User : Client
+{
+    public User()
+    {
+        _accessHandler = new HasReputation();
+    }
+    public int Reputation {  get; set; }
+    public override bool HandleAccess()
+    {
+        return _accessHandler.GetAccess(reputation: Reputation);
+    }
+}
+
+public class Manager : Client
+{
+    public Manager()
+    {
+        _accessHandler = new HasAccessAutomatic();
+    }
+}
+
+public class Admin: Client
+{
+    public Admin()
+    {
+        _accessHandler = new HasAccessAutomatic();
+    }
+}
+
+public interface IAccessHandler
+{
+    public bool GetAccess(int? reputation = 0, bool accessDisabled = false);
+}
+
+public class HasReputation: IAccessHandler
+{
+    public bool GetAccess(int? reputation - 0, bool accessDisabled = false)
+    {
+        return reputation > 20;
+    }
+
+}
+
+public class HasAccessAutomatic: IAccessHandler
+{
+    public bool GetAccess(int? reputation = 0, bool accessDisabled = false)
+    {
+        return !accessDisabled;
+    }
+}
